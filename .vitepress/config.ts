3e03eb3c4a52f7d1c12fed15f3c88c3013e2a61d
GOTO Ritsuki <develop@mizucoffee.net>
2024-01-01 12:23:14 +0900

    feat: init project

---
+++ .vitepress/config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "水珈琲の日誌",
+  description: "気になったことなんでも屋"
+})
---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+import { data as archives } from '../../archives.data.ts'
+
+const { site, frontmatter } = useData()
+</script>
+
+<template>
+  <header>
+    <h1>
+      <a href="/">
+        <img src="/logo.png" :alt="site.title">
+      </a>
+    </h1>
+    <p>{{ site.description }}</p>
+  </header>
+  <nav>
+    <a href="/">ホーム</a>
+    <a href="/about">水珈琲の日誌について</a>
+    <a href="/recommended">おすすめ記事</a>
+    <a href="/privacy-policy">プライバシーポリシー</a>
+  </nav>
+  <div v-if="frontmatter.home">
+    <ul>
+      <li v-for="post of archives">
+        <a :href="post.url">{{ post.frontmatter.title }}</a>
+        <span>by {{ post.frontmatter.author }}</span>
+      </li>
+    </ul>
+  </div>
+  <div v-else>
+    <a href="/">Home</a>
+    <Content />
+  </div>
+</template>
